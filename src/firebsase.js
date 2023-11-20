5978a70a68a5f7485923569cf6795f441829d8d2
Rahul <rahulprasad4work@gmail.com>
2023-11-20 14:18:52 +0530

    final deployment

---
+++ src/firebsase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClm-oCtoTtQTP4XIBBuSdwZ9722x_DGoI",
+  authDomain: "kanbanboard-877ae.firebaseapp.com",
+  projectId: "kanbanboard-877ae",
+  storageBucket: "kanbanboard-877ae.appspot.com",
+  messagingSenderId: "332613313340",
+  appId: "1:332613313340:web:b3be32724b385751fdf439"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
